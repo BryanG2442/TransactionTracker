abcc078f9dac7a8453f69196dc603e6bdde142fa
BryanG2442 <bryangarcia2442@gmail.com>
2024-10-15 14:54:45 -0700

    Added reports menu, can now show current months transactions

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,6 +1,9 @@
 package com.pluralsight;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -81,6 +84,7 @@ public class Main {
     }
     //Ledger menu
     public static void ledgerMenu(Scanner scanner) {
+        ArrayList<Transactions> transactionsArrayList = new ArrayList<Transactions>(getTransactions());
         String choice;
         do {
             System.out.println("Welcome to the Ledger menu. Please choose an option." +
@@ -93,17 +97,18 @@ public class Main {
             switch (choice){
                 case "A":
                 case "a":
-                    showTransactions(getTransactions());
+                    showTransactions(transactionsArrayList);
                     break;
                 case "D":
                 case "d":
-                    showDeposits(getTransactions());
+                    showDeposits(transactionsArrayList);
                     break;
                 case "P":
                 case "p":
-                    showPayments(getTransactions());
+                    showPayments(transactionsArrayList);
                 case "R":
                 case "r":
+                    reportsMenu(scanner, transactionsArrayList);
                     break;
                 case "H":
                 case "h":
@@ -115,6 +120,44 @@ public class Main {
         }   while (!choice.equalsIgnoreCase("H"));
     }
 
+
+    //Reports menu
+    public static void reportsMenu(Scanner scanner, ArrayList<Transactions> transactions){
+        String choice;
+        do {
+            System.out.println("Welcome to the reports menu. Please choose an option." +
+                    "\nPress 1 for all entries for the current month" +
+                    "\nPress 2 for all entries in the previous month" +
+                    "\nPress 3 for all entries in the current year" +
+                    "\nPress 4 for all entries in the previous year" +
+                    "\nPress 5 to search entries by vendor" +
+                    "\nPress 0 to return to the reports menu");
+            choice = scanner.nextLine();
+            switch (choice){
+                case "1":
+                    showCurrentMonthTransactions(transactions);
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "0":
+                    break;
+                default:
+                    System.out.println("Please input a valid option");
+                    break;
+            }
+        } while (!choice.equalsIgnoreCase("0"));
+
+    }
+
+    public static void  showCurrentMonthTransactions (ArrayList<Transactions> transactionsArrayList){
+
+        for (int i = transactionsArrayList.size(); i > 0; i--) {
+            if (LocalDate.now().getMonth() == LocalDate.parse(transactionsArrayList.get(0).getDate(), DateTimeFormatter.ofPattern("yyyy-MM-dd")).getMonth())
+                    transactionsArrayList.get(i-1).printInfo();
+        }
+        System.out.println();
+    }
 
     //Gets the current list of transactions and puts it into an array
     public static ArrayList<Transactions> getTransactions(){
@@ -129,6 +172,7 @@ public class Main {
                 transactionsList.add(new Transactions(objectVariables[0], objectVariables[1], objectVariables[2], objectVariables[3], Float.parseFloat(objectVariables[4])));
 
             }
+            readFile.close();
         } catch (IOException e) {
             System.out.println("Error reading file has occurred");
             e.printStackTrace();
@@ -136,6 +180,8 @@ public class Main {
 
         return transactionsList;
     }
+
+
     //A basic loop to show all transactions
     public static void showTransactions (ArrayList<Transactions> transactionList) {
         for (int i = transactionList.size(); i > 0; i--) {
@@ -144,6 +190,8 @@ public class Main {
         System.out.println();
     }
 
+
+    //Slightly edited loop to show all positive amount values (deposits)
     public static void showDeposits (ArrayList<Transactions> transactionList) {
         for (int i = transactionList.size(); i > 0; i--) {
             if (transactionList.get(i-1).getAmount() > 0) {
@@ -153,6 +201,9 @@ public class Main {
         System.out.println();
     }
 
+
+
+    //Slightly edited loop to show all negative amount values (payments)
     public static void showPayments (ArrayList<Transactions> transactionList) {
         for (int i = transactionList.size(); i > 0; i--) {
             if (transactionList.get(i-1).getAmount() < 0) {
