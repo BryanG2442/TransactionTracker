f3bd45ae6ee08131f00ebc6711519caf58a8bdeb
BryanG2442 <bryangarcia2442@gmail.com>
2024-10-15 10:25:43 -0700

    Can add deposit entries

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,8 +1,12 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    final static String FILEPATH = "src/transaction.csv";
     public static void main(String[] args) {
         System.out.println("Welcome to this transaction application.");
         homeScreen();
@@ -25,17 +29,49 @@ public class Main {
                     "to exit the program, input X");
             choice = scanner.nextLine();
             switch (choice){
-                case "D": break;
+                case "D":
+                    makeDeposit(scanner);
+                    break;
                 case "P": break;
                 case "L": break;
                 case "X": break;
                 default:
                     System.out.println("\nInvalid input. Please try a valid option");
         }} while (!(choice.equalsIgnoreCase("X")));
-
+    scanner.close();
 
 
     }
+
+    public static void makeDeposit(Scanner scanner) {
+        System.out.print("Please input the deposit source: ");
+        String source = scanner.nextLine();
+        System.out.print("Please input the buyer: ");
+        String buyer = scanner.nextLine();
+        System.out.print("Please input the deposit amount: ");
+        double price = scanner.nextDouble();
+        Transactions transactions = new Transactions();
+        String toFile = String.format("%S|%S|%S|%S|%.2f",transactions.getDate(), transactions.getTime(), source, buyer, price);
+        writeToFile(toFile);
+        scanner.nextLine();
+
+    }
+
+
+
+    public static void writeToFile(String _input){
+        try {
+            FileWriter fileWriter = new FileWriter(FILEPATH, true);
+            BufferedWriter outputFile = new BufferedWriter(fileWriter);
+            outputFile.write(_input);
+            outputFile.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file");
+        }
+}
+
+
+
 
 
 
--- src/main/java/com/pluralsight/Transactions.java
+++ src/main/java/com/pluralsight/Transactions.java
@@ -1,24 +1,35 @@
 package com.pluralsight;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Transactions {
-    private LocalDateTime currentTime;
+    private static LocalDateTime currentTime;
+    private static DateTimeFormatter dateFormat;
     private String description;
     private String vendor;
     private double amount;
 
 
-    public Transactions(LocalDateTime _currentTime, String _description, String _vendor, double _amount){
-        currentTime = _currentTime;
+    public Transactions(String _description, String _vendor, double _amount){
         description = _description;
         vendor = _vendor;
         amount = _amount;
     }
     public Transactions(){}
 
+    public String getDate(){
+        dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        currentTime = LocalDateTime.now();
+        return  currentTime.format(dateFormat);
+    }
 
-
+    public String getTime(){
+        dateFormat = DateTimeFormatter.ofPattern("HH-mm-ss");
+        currentTime = LocalDateTime.now();
+        return currentTime.format(dateFormat);
+    }
 
 
 
