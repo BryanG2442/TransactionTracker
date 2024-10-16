54a449b362364a1f3dea8c0cb0b00599a9896fd5
BryanG2442 <bryangarcia2442@gmail.com>
2024-10-16 11:33:14 -0700

    Minor edit

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -15,9 +15,6 @@ public class Main {
         System.out.println("Welcome to this transaction application.");
         homeScreen();
         System.out.println("Thank you for using our application.");
-
-
-
     }
 
     //The default screen the program goes through.
